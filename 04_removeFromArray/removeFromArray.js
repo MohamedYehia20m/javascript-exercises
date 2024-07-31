ca93c1cf38ac5f7b973b010bd62f73c6c3081baa
MohamedYehia20m <77248801+MohamedYehia20m@users.noreply.github.com>
2024-07-31 18:01:51 +0300

    fix 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
 const removeFromArray = function() {
+    let len = arguments.length;
+
+    for (let i = 1 ;i < len ;i++)
+    {
+        arguments[0] = arguments[0].filter(element => element !== arguments[i]);
+    }
+
+    return arguments[0];
 };
 
 // Do not edit below this line
