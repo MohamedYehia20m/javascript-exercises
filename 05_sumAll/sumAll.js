d7451e801c12a85849e45d239110171d83355a79
MohamedYehia20m <77248801+MohamedYehia20m@users.noreply.github.com>
2024-07-31 18:39:12 +0300

    fix 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,12 @@
-const sumAll = function() {
+const sumAll = function(start ,end) {
+    if ((Number.isInteger(start) && start > 0) && (Number.isInteger(end) && end > 0))
+    {
+        return (start + end)*(Math.max(start,end)/2);
+    }
+    else
+    {
+        return 'ERROR'
+    }
 
 };
 
