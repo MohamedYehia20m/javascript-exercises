989ac36fdf409a58759eb5cae193149d98cfbedc
MohamedYehia20m <77248801+MohamedYehia20m@users.noreply.github.com>
2024-07-31 19:08:58 +0300

    fix 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Temp_F) {
+  const stringNumber = ((Temp_F - 32) / 1.8).toFixed(1);
+  return Number(stringNumber)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Temp_C) {
+  const stringNumber = ((Temp_C * 1.8) + 32).toFixed(1);
+  return Number(stringNumber)
 };
 
 // Do not edit below this line
