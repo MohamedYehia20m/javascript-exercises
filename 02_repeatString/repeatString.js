e559b21121843cfc87b5f5f59f3d8abbb695053b
MohamedYehia20m <77248801+MohamedYehia20m@users.noreply.github.com>
2024-07-31 17:18:47 +0300

    fix 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str , NoTimes) {
+    if (NoTimes < 0) {
+        return 'ERROR'
+    }
+    new_str = '';
 
+    for (let i = 0 ;i < NoTimes ;i++)
+    {
+        new_str = new_str.concat(str);
+    }
+
+    return new_str
 };
 
 // Do not edit below this line
